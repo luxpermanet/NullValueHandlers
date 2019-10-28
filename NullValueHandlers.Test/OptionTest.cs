675c347f74f9b6198d7b3dbf5f83a9bc3123c7fa
ERGO <ergo@simcorp.com>
2019-10-28 15:58:29 +0100

    Initial-commit

---
+++ NullValueHandlers.Test/OptionTest.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NullValueHandlers.Test {
+    [TestClass]
+    public class OptionTest {
+        [TestMethod]
+        public void HasValueShouldReturnFalseWhenOptionHasNoValueTest() {
+            // Arrange
+            var option = Option<TestClass>.None;
+
+            // Assert
+            Assert.IsFalse(option.HasValue);
+        }
+
+        [TestMethod]
+        public void OptionShouldNotEnumerateWhenItHasNoValueTest() {
+            // Arrange
+            var option = Option<TestClass>.None;
+
+            // Act
+            bool canEnumerate = false;
+            foreach (TestClass unused in option) {
+                canEnumerate = true;
+            }
+
+            // Assert
+            Assert.IsFalse(canEnumerate);
+        }
+
+        [TestMethod]
+        public void ValueShouldThrowInvalidOperationExceptionWhenOptionHasNoValueTest() {
+            // Arrange
+            var option = Option<TestClass>.None;
+
+            // Act
+            bool throws = ThrowsExact<InvalidOperationException>(() => {
+                TestClass unused = option.Value;
+            });
+
+            // Assert
+            Assert.IsTrue(throws);
+        }
+
+        [TestMethod]
+        public void SingleShouldShouldThrowNullExceptionWhenParameterIsNullTest() {
+            // Arrange
+            bool throws = ThrowsExact<ArgumentNullException>(() => {
+                var unused = Option<TestClass>.Single(null);
+            });
+
+            // Assert
+            Assert.IsTrue(throws);
+        }
+
+        [TestMethod]
+        public void HasValueShouldReturnTrueWhenOptionHasValueTest() {
+            // Arrange
+            var option = Option<TestClass>.Single(new TestClass());
+
+            // Assert
+            Assert.IsTrue(option.HasValue);
+        }
+
+        [TestMethod]
+        public void ValueShouldReturnIntrinsicValueWhenOptionHasValueTest() {
+            // Arrange
+            TestClass expectedValue = new TestClass();
+            var option = Option<TestClass>.Single(expectedValue);
+
+            // Act
+            TestClass value = option.Value;
+
+            // Assert
+            Assert.AreSame(expectedValue, value);
+        }
+
+        [TestMethod]
+        public void OptionShouldEnumerateWhenItHasValueTest() {
+            // Arrange
+            var option = Option<TestClass>.Single(new TestClass());
+
+            // Act
+            bool canEnumerate = false;
+            foreach (TestClass unused in option) {
+                canEnumerate = true;
+            }
+
+            // Assert
+            Assert.IsTrue(canEnumerate);
+        }
+
+        private class TestClass { }
+
+        /// <summary>
+        /// Determines if the provided action throws the expected exact exception type
+        /// </summary>
+        /// <typeparam name="TException">Expected exception type</typeparam>
+        /// <param name="action">Action to test for expected exception</param>
+        /// <exception cref="ArgumentNullException">Throws when the action parameter is null</exception>
+        public static bool ThrowsExact<TException>(Action action) where TException : Exception {
+            if (action == null) {
+                throw new ArgumentNullException(nameof(action));
+            }
+
+            try {
+                action();
+            } catch (Exception ex) {
+                if (ex.GetType() == typeof(TException)) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Async version of <see cref="ThrowsExact{TException}"/>
+        /// </summary>
+        private static async Task<bool> ThrowsExactAsync<TException>(Func<Task> action) where TException : Exception {
+            if (action == null) {
+                throw new ArgumentNullException(nameof(action));
+            }
+
+            try {
+                await action();
+            } catch (Exception ex) {
+                if (ex.GetType() == typeof(TException)) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Determines if the provided action throws the expected exception type
+        /// <para>NOTE that this will work for derived exception types as well. If you do not want this behaviour use <see cref="ThrowsExact{TException}"/> instead</para>
+        /// </summary>
+        /// <typeparam name="TException">Expected exception type</typeparam>
+        /// <param name="action">Action to test for expected exception</param>
+        /// <exception cref="ArgumentNullException">Throws when the action parameter is null</exception>
+        private static bool Throws<TException>(Action action) where TException : Exception {
+            if (action == null) {
+                throw new ArgumentNullException(nameof(action));
+            }
+
+            try {
+                action();
+            } catch (TException) {
+                return true;
+            } catch (Exception) {
+                // ignored
+            }
+
+            return false;
+        }
+    }
+}
---
+++ NullValueHandlers/Option.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace NullValueHandlers {
+    /// <summary>
+    /// Wrapper for a null or single object to avoid null checks on the caller side.
+    /// <para>Use <see cref="None"/> to signal the caller that the method returned a null object.</para>
+    /// <para>Use <see cref="Single"/> to signal the caller that the method returned a valid object.</para>
+    /// <para>Since this type implements <see cref="IEnumerable{T}"/>, the caller may use LINQ methods to access the value.</para>
+    /// </summary>
+    public class Option<T> : IEnumerable<T> where T : class {
+        /// <summary>
+        /// Creates an <see cref="Option{T}"/> with no value.
+        /// </summary>
+        public static readonly Option<T> None = new Option<T>(new T[0]);
+
+        private readonly T[] Data;
+
+        /// <summary>
+        /// Determines whether the <see cref="Option{T}"/> has value or not.
+        /// </summary>
+        public bool HasValue => Data.Length > 0;
+
+        /// <summary>
+        /// Gets <see cref="Option{T}"/>'s value.
+        /// <para>Caution: Call for <see cref="HasValue"/> first prior to calling to this property.</para>
+        /// </summary>
+        /// <exception cref="InvalidOperationException">When the <see cref="Option{T}"/> has no value.</exception>
+        public T Value {
+            get {
+                if (!HasValue) {
+                    throw new InvalidOperationException("Option has no value");
+                }
+                return Data[0];
+            }
+        }
+
+        private Option(T[] data) {
+            Data = data;
+        }
+
+        /// <summary>
+        /// Creates an <see cref="Option{T}"/> with a valid object.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">Throws when argument provided is a null object</exception>
+        public static Option<T> Single(T value) {
+            if (value == null) { throw new ArgumentNullException(nameof(value)); }
+
+            return new Option<T>(new[] { value });
+        }
+
+        public IEnumerator<T> GetEnumerator() {
+            return ((IEnumerable<T>)Data).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return GetEnumerator();
+        }
+    }
+}
